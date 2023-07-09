fbd34304c431d2d29af37ac2c7bff4e3d156eee1
JamesMcGuigan <james.mcguigan@gmail.com>
2023-07-09 08:40:48 +0100

    _build.sh | requirements.txt without venv

--- _build.sh
+++ _build.sh
@@ -3,7 +3,6 @@
 
 # python3 -m venv venv
 # source ./venv/bin/activate
-# python3 -m pip install -r requirements.txt
-
+python3 -m pip install -r requirements.txt
 tar xvzf .install/quarto-1.3.433-linux-amd64.tar.gz -C .install
 .install/quarto-1.3.433/bin/quarto render .
