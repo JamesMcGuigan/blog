3e35cfbfc7f88979347301b99d6503aef2881386
JamesMcGuigan <james.mcguigan@gmail.com>
2023-07-09 08:39:52 +0100

    _build.sh | without requirements.txt

--- _build.sh
+++ _build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Build/Deploy instructions for Vercel
 
-python3 -m venv venv
-source ./venv/bin/activate
-python3 -m pip install -r requirements.txt
+# python3 -m venv venv
+# source ./venv/bin/activate
+# python3 -m pip install -r requirements.txt
 
 tar xvzf .install/quarto-1.3.433-linux-amd64.tar.gz -C .install
 .install/quarto-1.3.433/bin/quarto render .
