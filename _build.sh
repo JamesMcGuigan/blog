d389abd625e7aee45032c838f1e0ff6774956d94
JamesMcGuigan <james.mcguigan@gmail.com>
2023-07-20 05:38:12 +0100

    _build.sh | docs + tar xzf

--- _build.sh
+++ _build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# Build/Deploy instructions for Vercel
-# BROKEN: https://github.com/orgs/vercel/discussions/3165
+# Quarto CICD Build/Deploy Script
+# Netify: WORKS
+# Vercel: https://github.com/orgs/vercel/discussions/3165
 
 set -x
 pwd
@@ -9,5 +10,5 @@ ls -la
 # python3 -m venv venv
 # source ./venv/bin/activate
 python3 -m pip install -r requirements.txt
-tar xvzf .install/quarto-1.3.433-linux-amd64.tar.gz -C .install
+tar xzf .install/quarto-1.3.433-linux-amd64.tar.gz -C .install
 .install/quarto-1.3.433/bin/quarto render .
