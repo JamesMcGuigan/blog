0ddbef2908ddd30ace933c2a34a6d0245976ebbd
James McGuigan <james.mcguigan@gmail.com>
2023-07-23 22:24:38 +0000

    Built site for gh-pages

---
+++ _build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Quarto CICD Build/Deploy Script
+# Netify: WORKS
+# Vercel: WORKS: https://github.com/orgs/vercel/discussions/3165
+
+set -x
+pwd
+ls -la
+
+# python3 -m venv venv
+# source ./venv/bin/activate
+python3 -m pip install -r requirements.txt
+tar xzf .install/quarto-1.3.433-linux-amd64.tar.gz -C .install
+.install/quarto-1.3.433/bin/quarto render .
